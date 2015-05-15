6f2f82787fa3c9d1735ee092076a9e03238cd78c
alexey-shapilov <alexey.shapilov@gmail.com>
2015-05-15 20:03:37 +0300

    Инициализация

---
+++ README.md
@@ -0,0 +1,4 @@
+guest_book
+==========
+
+A Symfony project created on May 12, 2015, 4:25 pm.
---
+++ src/Alexus/GuestBookBundle/AlexusGuestBookBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Alexus\GuestBookBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class AlexusGuestBookBundle extends Bundle
+{
+}
